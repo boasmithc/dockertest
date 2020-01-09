c28d168457dd1f8afd204036841afe907674d4c8
Ubuntu <ubuntu@ip-172-31-16-143.us-east-2.compute.internal>
2020-01-09 19:45:08 +0000

    Added a volume to the mysql container

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,4 @@
 docker network create my_network
 docker run -d --net=my_network --name app_container  -p 5000:5000 flask_app
-docker run -d --net=my_network --name db_container -e MYSQL_ROOT_PASSWORD=my-secret-pw  mysql
+docker run -d --net=my_network --name db_container -v ~/my_web_app/mydata:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw  mysql
 docker run -it --net=my_network --name shell_container alpine
